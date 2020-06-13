e0df7cd8d7a6434163df5be16447f703a23a5fcc
vgaimanovskyi <vgaymanovskiy@gmail.com>
2020-06-13 11:06:16 +0300

    add ie crossbrowser

--- js/ie.js
+++ js/ie.js
@@ -3,13 +3,13 @@ $(function () {
     if (!!window.MSInputMethodContext && !!document.documentMode) {
         $("html").addClass("ie11");
     }
-    /* if ($.browser.msie && $.browser.version == 10) {
+    if (navigator.userAgent.toLowerCase().indexOf('msie 10') != -1) {
         $("html").addClass("ie10");
     }
-    if ($.browser.msie && $.browser.version == 9) {
+    if (navigator.userAgent.toLowerCase().indexOf('msie 9') != -1) {
         $("html").addClass("ie9");
     }
-    if ($.browser.msie && $.browser.version == 8) {
+    if (navigator.userAgent.toLowerCase().indexOf('msie 8') != -1) {
         $("html").addClass("ie8");
-    } */
+    }
 });
